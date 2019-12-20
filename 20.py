a79c5432fcd169575d585b1dfde5eadb1f868c2c
BuildTools <unconfigured@null.spigotmc.org>
2019-12-20 22:45:50 +0000

    day 20

--- 20.py
+++ 20.py
@@ -19,35 +19,35 @@ def parse_input(filename):
 
 def part_1_and_2(data):
     outputs = []
+    height = len(data)
+    width = len(data[0])
+    portal_letters = {}
+    for y in range(height):
+        for x in range(width):
+            char = data[y][x]
+            if char.isalpha():
+                for dx, dy in DELTAS:
+                    if (x + dx) < 0 or (x + dx) >= width or (y + dy) < 0 or (y + dy) >= height:
+                        continue
+                    adj_char = data[y + dy][x + dx]
+                    if adj_char in ('#', '.'):
+                        code = data[y - dy][x - dx] + data[y][x]
+                        if dy == - 1 or dx == -1:
+                            code = code[::-1]
+                        if code not in portal_letters:
+                            portal_letters[code] = []
+                        portal_letters[code].append((x + dx, y + dy))
+    start = portal_letters['AA'][0]
+    end = portal_letters['ZZ'][0]
+    portals = {}
+    for key in portal_letters:
+        if len(portal_letters[key]) != 2:
+            continue
+        portals[portal_letters[key][0]] = portal_letters[key][1]
+        portals[portal_letters[key][1]] = portal_letters[key][0]
+    start = (start[0], start[1], 0)
+    target = (end[0], end[1], 0)
     for part in (1, 2):
-        height = len(data)
-        width = len(data[0])
-        portal_letters = {}
-        for y in range(height):
-            for x in range(width):
-                char = data[y][x]
-                if char.isalpha():
-                    for dx, dy in DELTAS:
-                        if (x + dx) < 0 or (x + dx) >= width or (y + dy) < 0 or (y + dy) >= height:
-                            continue
-                        adj_char = data[y + dy][x + dx]
-                        if adj_char in ('#', '.'):
-                            code = data[y - dy][x - dx] + data[y][x]
-                            if dy == - 1 or dx == -1:
-                                code = code[::-1]
-                            if code not in portal_letters:
-                                portal_letters[code] = []
-                            portal_letters[code].append((x + dx, y + dy))
-        start = portal_letters['AA'][0]
-        end = portal_letters['ZZ'][0]
-        portals = {}
-        for key in portal_letters:
-            if len(portal_letters[key]) != 2:
-                continue
-            portals[portal_letters[key][0]] = portal_letters[key][1]
-            portals[portal_letters[key][1]] = portal_letters[key][0]
-        start = (start[0], start[1], 0)
-        target = (end[0], end[1], 0)
         node_queue = [start]
         visited = []
         prev = {}
